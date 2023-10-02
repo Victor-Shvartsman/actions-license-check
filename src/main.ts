b89b28ab34e194340b4311cf44543c3cb571bc53
Victor Shvartsman <victor.shvartsman@cardinalops.com>
2023-10-02 15:59:38 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import * as core from '@actions/core';
 import * as fs from 'fs';
 import path from 'path';
 import { runLicenseCheck } from './runLicenseCheck';
+import { stdout } from 'process';
 
 async function run() {
   try {
 
       const allowedLicenses = core.getInput('allowed-licenses');
       //const allowedLicenses = "MIT; ISC; Apache-2.0; BlueOak-1.0.0"
+      //const allowedLicenses = "MIT"
       const subDir = core.getInput('sub-dir');
       //const subDir = '.';
       const output = await runLicenseCheck({ allowedLicenses, subDir });
@@ -16,13 +18,7 @@ async function run() {
         path.join('license-check', 'index.html'),
         `<html><body><pre><code>${output}</code></pre></body></html>`
       );
-
-      return {
-        metadata: JSON.parse(output),
-        isOkay: true,
-        shortText: 'All used licenses are allowed',
-        text: output
-      };
+      core.info('All used licenses are allowed')
 
   } catch (error: any) {
     core.setFailed(error.message);
@@ -32,6 +28,7 @@ async function run() {
       `<html><body><pre><code>${error.message}</code></pre></body></html>`
     );
     core.info('output written to license-check');
+    
   }
 }
 
